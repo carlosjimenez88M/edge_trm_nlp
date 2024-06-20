9be27b134952c5cf9f26d23e9f7a258fac1c4e58
carlos_raspberrypi5 <danieljimenez88m@gmail.com>
2024-06-19 19:54:06 -0500

    Code adaptation into Raspberry pi

--- notebook/news_web_scrapping.py
+++ notebook/news_web_scrapping.py
@@ -7,11 +7,12 @@
 
 
 # Description ----------------
-#
-#
+# The principal goal in this script is create a program to scrap from google news  
+# 
 
 
 # Libraries --------------------
+import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -20,81 +21,29 @@ from datetime import datetime, timedelta
 import pytz
 
 
-
-
-## Program ------------------
-
-url = 'https://news.google.com/topics/CAAqLAgKIiZDQkFTRmdvSUwyMHZNRFZxYUdjU0JtVnpMVFF4T1JvQ1EwOG9BQVAB?hl=es-419&gl=CO&ceid=CO%3Aes-419'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html5lib')
-
-
-## Title
-news_links = soup.find_all('a', class_='gPFEn')
-#print(news_links)
-
-print('=='*32)
-print(news_links[0].get_text(strip=True).strip())
-
-## Source
-print('=='*32)
-sources= soup.find_all('div',class_='MCAGUe')
-print(sources[0].get_text(strip=True).replace('Más','').strip())
-
-
-## Publication time
-print('=='*32)
-times= soup.find_all('time', class_='hvbAAd')
-full_datetime = times[0]
-lag_time = full_datetime.get_text(strip=True)
-lag_time = int(re.findall(r'\d+', lag_time)[0])
-#print(lag_time)
-
-
-local_tz = pytz.timezone('America/Bogota')  # Adjust to your local timezone
-datetime_local = datetime.now(local_tz)
-time_diff = timedelta(hours=lag_time)
-hms_news = datetime_local- time_diff
-print(hms_news.time().strftime("%H:%M:%S"))
-print()
-
-
-
-
-### Function ------------
-
 def go(timezone='America/Bogota'):
     url = 'https://news.google.com/topics/CAAqLAgKIiZDQkFTRmdvSUwyMHZNRFZxYUdjU0JtVnpMVFF4T1JvQ1EwOG9BQVAB?hl=es-419&gl=CO&ceid=CO%3Aes-419'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html5lib')
 
-
     news_links = soup.find_all('a', class_='gPFEn')
     sources = soup.find_all('div', class_='MCAGUe')
     times = soup.find_all('time', class_='hvbAAd')
 
-
     num_items = min(len(news_links), len(sources), len(times))
 
-
     data = []
-
 
     local_tz = pytz.timezone(timezone)
 
     for i in range(num_items):
-
         title = news_links[i].get_text(strip=True).strip()
-
-
         source = sources[i].get_text(strip=True).replace('Más', '').strip()
-
 
         full_datetime = times[i]
         lag_time_text = full_datetime.get_text(strip=True)
         lag_time_match = re.findall(r'\d+', lag_time_text)
 
-        # Verificar si se encontró un número en lag_time_text
         if lag_time_match:
             lag_time = int(lag_time_match[0])
             datetime_local = datetime.now(local_tz)
@@ -102,8 +51,7 @@ def go(timezone='America/Bogota'):
             publication_time = datetime_local - time_diff
             time_part = publication_time.time().strftime("%H:%M:%S")
         else:
-            time_part = "N/A"  # Si no se encuentra un número, asignar un valor predeterminado
-
+            time_part = "N/A"
 
         data.append({
             'Title': title,
@@ -111,37 +59,23 @@ def go(timezone='America/Bogota'):
             'Publication Time': time_part
         })
 
-
     df = pd.DataFrame(data)
     return df
 
-def quote(value):
-    return f"""'{str(value).replace("'", "''")}'"""
+def save_to_parquet_file(df, filename=None):
+    save_directory = '/mnt/external/Raspberry/news_project'
+    os.makedirs(save_directory, exist_ok=True)
 
-def save_to_sql_file(df, filename=None):
     if filename is None:
-        # Generar el nombre del archivo con la fecha actual
         current_date = datetime.now().strftime('%Y-%m-%d')
-        filename = f'news_data_{current_date}.sql'
+        filename = f'{save_directory}/news_data_{current_date}.parquet'
 
-    with open(filename, 'w') as f:
-        f.write('CREATE TABLE IF NOT EXISTS news (\n')
-        f.write('    title TEXT PRIMARY KEY,\n')
-        f.write('    source TEXT,\n')
-        f.write('    publication_time TEXT,\n')
-        f.write(');\n\n')
-
-        for index, row in df.iterrows():
-            f.write('INSERT INTO news (title, source, publication_time, url) VALUES (\n')
-            f.write(f"    {quote(row['Title'])},\n")
-            f.write(f"    {quote(row['Source'])},\n")
-            f.write(f"    {quote(row['Publication Time'])},\n")
-            f.write(');\n')
+    # Guardar el DataFrame en formato Parquet
+    df.to_csv(filename, index=False)
 
 if __name__ == '__main__':
     df = go()
-    save_to_sql_file(df)
-
+    save_to_parquet_file(df)
     print(df.head(15))
 
 
@@ -166,3 +100,5 @@ if __name__ == '__main__':
 # Arreglar que cuando salga ayer en el tiempo hacer una resta de 24 horas.
 
 
+#ls /mnt/external/Raspberry/news_project
+#pip install pandas requests beautifulsoup4 html5lib pytz
