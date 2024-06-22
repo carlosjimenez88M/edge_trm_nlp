94690622064d6c52d407b9448389a6aec0222ddd
Carlos Jimenez <carlosdaniel.jimenez@globant.com>
2024-06-21 23:30:42 -0500

    model scraping with CI and release

--- Pipeline/run.sh
+++ Pipeline/run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#!/bin/bash
 
+# Asegúrate de que este script se ejecute en el entorno correcto
+if [ -f "/Users/danieljimenez/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+    source /Users/danieljimenez/opt/anaconda3/etc/profile.d/conda.sh
+else
+    source ~/conda/etc/profile.d/conda.sh
+fi
 
-source /Users/danieljimenez/opt/anaconda3/etc/profile.d/conda.sh
 conda activate edge_trm_nlp
 
-
+# Establecer la variable de entorno HYDRA_FULL_ERROR
 export HYDRA_FULL_ERROR=1
 
-
+# Ejecutar mlflow run con una ruta relativa
 mlflow run .
