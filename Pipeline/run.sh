bf62330b434309495f731d4ac82f6f967124fd6e
Carlos Jimenez <carlosdaniel.jimenez@globant.com>
2024-06-21 21:41:33 -0500

    Adapting scraping.yml

---
+++ Pipeline/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /Users/danieljimenez/opt/anaconda3/etc/profile.d/conda.sh
+conda activate edge_trm_nlp
+mlflow run /Users/danieljimenez/Documents/Projects/Personal_Projects/edge_trm_nlp/Pipeline
